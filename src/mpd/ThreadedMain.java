0f90dd34026a61604d52f205d7506fda3f2784db
walte774 <walte774@morris.umn.edu>
2019-11-07 11:32:28 -0600

    Write ThreadedMinimumPairwiseDistance
    This is a partial implementation. We believe the problem is in our loop condition(s) of the thread for MidLeft.
    
     Co-authored-by: Melissa Kamrowski <melissarae163@gmail.com>

--- src/mpd/ThreadedMain.java
+++ src/mpd/ThreadedMain.java
@@ -5,15 +5,18 @@ import java.util.Random;
 public class ThreadedMain {
 
     public static void main(String[] args) {
-        int numValues = Integer.parseInt(args[0]);
+        //int numValues = Integer.parseInt(args[0]);
+        int numValues = 10;
         MinimumPairwiseDistance mpd = new ThreadedMinimumPairwiseDistance();
 
         Random random = new Random();
         int[] values = new int[numValues];
         for (int i = 0; i < numValues; ++i) {
             values[i] = random.nextInt();
+            System.out.println(values[i]);
+
         }
-        
+
         long result = mpd.minimumPairwiseDistance(values);
         System.out.println("The minimum distance was " + result);
     }
--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,105 @@
 package mpd;
 
+import java.sql.SQLOutput;
+
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+
+    int[] values;
+    long globalResult;
+    int N;
 
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+        this.values = values;
+        N = this.values.length;
+        Thread[] threads = new Thread[4];
+        threads[0] = new Thread(new LowerLeft());
+        threads[1] = new Thread(new MidLeft());
+        threads[2] = new Thread(new MidRight());
+        threads[3] = new Thread(new TopRight());
+        try {
+            for (int i = 0; i < 4; i++) {
+                threads[i].start();
+            }
+            for (int i = 0; i < 4; i++) {
+                threads[i].join();
+            }
+        } catch (InterruptedException ie) {
+            System.out.println("Thread was interrupted");
+            System.err.println(ie);
+        }
+        return globalResult;
     }
 
+    private class LowerLeft implements Runnable {
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = 0; i < N/2; i++) {
+                System.out.println(i);
+                System.out.println(values[i]);
+                for (int j = 0; j < i; j++) {
+                    System.out.println(j);
+                    System.out.println(values[j]);
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+            if (localResult < globalResult) {
+                globalResult = localResult;
+            }
+        }
+    }
+
+    private class MidLeft implements Runnable {
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int j = 0; j < N/2; j++) {
+                for (int i = N/2; i <= j + N/2; j++) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+            if (localResult < globalResult) {
+                globalResult = localResult;
+            }
+        }
+    }
+
+    private class MidRight implements Runnable {
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = N/2; i < N; ++i) {
+                for (int j = N/2; j + N/2 < i; ++j) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+            if (localResult < globalResult) {
+                globalResult = localResult;
+            }
+        }
+    }
+
+    private class TopRight implements Runnable {
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = N/2; i < N; ++i) {
+                for (int j = N/2; j < i; ++j) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+            if (localResult < globalResult) {
+                globalResult = localResult;
+            }
+        }
+    }
 }
